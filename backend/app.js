17f897786dd4a3d25f9eec93e15a08f72e5aca6c
Michael Magen <michael.magen101@gmail.com>
2022-09-14 16:33:39 -0700

    Added user router that allows for adding and reading users

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-//const usersRouter = require('./controllers/users')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const morgan = require('morgan')
@@ -25,8 +25,7 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(middleware.tokenExtractor)
 
-//app.use('/api/users', usersRouter)
-
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ backend/controllers/users.js
@@ -0,0 +1,53 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+// get all of the users in the DB
+usersRouter.get('/', async(request, response) => {
+    //const users = await User.find({}).populate('lists').populate('items')
+    const users = await User.find({})
+    response.json(users)
+})
+
+usersRouter.get('/:id', async(request, response) => {
+    const user = await User.findById(request.params.id)
+
+    response.json(user)
+})
+// create a new user
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({
+      error: 'username must be unique'
+    })
+  }
+
+  if (!password) {
+    return response.status(400).json({
+        error: 'password can not be missing'
+    })
+  } 
+  else if (password.length < 7) {
+    return response.status(400).json({
+        error: 'password must be at least 7 characters'
+    })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
