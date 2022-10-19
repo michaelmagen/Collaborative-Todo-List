3463e5686a1f8c105017e15a7bffa4baf5fbca17
Michael Magen <michael.magen101@gmail.com>
2022-10-19 16:19:18 -0700

    Add ability for no alert to appear

--- frontend-collab-todo-list/src/components/Alert.js
+++ frontend-collab-todo-list/src/components/Alert.js
@@ -1,9 +1,14 @@
 import Alert from 'react-bootstrap/Alert';
 
-const AlertMessage = ({message, variant}) => (
-  <Alert variant={variant}> 
-    {message}
-  </Alert>
+const AlertMessage = ({message, variant}) =>  {
+  if (message === '' || variant === '') {
+    return
+  }
+  return (
+    <Alert variant={variant}> 
+      {message}
+    </Alert>
 )
+}
 
 export default AlertMessage
