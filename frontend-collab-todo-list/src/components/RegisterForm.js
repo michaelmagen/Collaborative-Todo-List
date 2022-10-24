d6d5229dbff591221e215df50d611ef5771f861e
Michael Magen <michael.magen101@gmail.com>
2022-10-23 18:38:35 -0700

    Migrate registration form to styled component

--- frontend-collab-todo-list/src/components/RegisterForm.js
+++ frontend-collab-todo-list/src/components/RegisterForm.js
@@ -1,37 +1,48 @@
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+
 const RegisterForm = (props) => (
   <>
-  <form onSubmit={props.handleRegistration}>
-    <div>
-      full name
-        <input
-        type="test"
-        value={props.name}
-        name="Password"
-        onChange={({ target }) => props.setName(target.value)}
-      />
-    </div>
-    <div>
-      username
-        <input
-        type="text"
-        value={props.username}
-        name="Username"
-        onChange={({ target }) => props.setUsername(target.value)}
-      />
-    </div>
-    <div>
-      password
-        <input
-        type="password"
-        value={props.password}
-        name="Password"
-        onChange={({ target }) => props.setPassword(target.value)}
-      />
-    </div>
-    <button type="submit">Create Account</button>
-  </form>      
-  <button onClick={() => props.setCreateUser(false)}> Login instead</button>
+    <Form>
+      <Form.Group className='mx-auto w-50 mb-2'>
+        <Form.Label>Full Name</Form.Label>
+        <Form.Control 
+          type='text' 
+          placeholder='First Last' 
+          value={props.name} 
+          onChange={({target}) => props.setName(target.value)} 
+        />
+      </Form.Group>
+      <Form.Group className="mx-auto w-50 mb-2">
+        <Form.Label>Username</Form.Label>
+        <Form.Control 
+          type="text" 
+          placeholder="username" 
+          value={props.username} 
+          onChange={({target}) => props.setUsername(target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mx-auto w-50 mb-2" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control 
+          type="password" 
+          placeholder="Password" 
+          value={props.password} 
+          onChange={({target}) => props.setPassword(target.value)}
+        />
+      </Form.Group>
+    </Form>
+    <Row className='w-25 mx-auto m-4'>
+      <Button onClick={props.handleRegistration} variant='primary'>
+        Create Account
+      </Button>
+    </Row>
+    <Row className='w-25 mx-auto m-4'>
+      <Button onClick={() => props.setCreateUser(false)} variant='outline-secondary'>
+        Back to Sign in
+      </Button>
+    </Row>
   </>
 )
-
 export default RegisterForm
