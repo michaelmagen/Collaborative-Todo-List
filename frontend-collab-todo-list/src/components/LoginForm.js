3f00d693bc0edee050a222e4f33515abc392f202
Michael Magen <michael.magen101@gmail.com>
2022-10-23 18:26:24 -0700

    Migrate to react-boostrap LoginForm
    Renamed listButton component file for clarity. Changed LoginForm to
    use react-boostrap components.

--- frontend-collab-todo-list/src/components/LoginForm.js
+++ frontend-collab-todo-list/src/components/LoginForm.js
@@ -1,27 +1,30 @@
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+
+
 const LoginForm = ({ handleLogin, username, password, setPassword, setUsername, setCreateUser}) => (
   <>
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-          <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-   <button onClick={() => setCreateUser(true)}> Sign Up</button>
+    <Form>
+      <Form.Group className="mx-auto w-50 mb-2">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" placeholder="username" value={username} onChange={({target}) => setUsername(target.value)}/>
+      </Form.Group>
+      <Form.Group className="mx-auto w-50 mb-2" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" value={password} onChange={({target}) => setPassword(target.value)}/>
+      </Form.Group>
+    </Form>
+  <Row className='w-25 mx-auto m-4'>
+      <Button onClick={handleLogin} variant='primary'>
+        Sign in
+      </Button>
+  </Row>
+  <Row className='w-25 mx-auto m-4'>
+      <Button onClick={() => setCreateUser(true)} variant='outline-secondary'>
+        Create Account
+      </Button>
+  </Row>
   </>
 )
 
