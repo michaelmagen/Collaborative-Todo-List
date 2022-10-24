86fd9ab763a021f9e29528c908c79fc7a1ef9b24
Michael Magen <michael.magen101@gmail.com>
2022-10-23 18:37:46 -0700

    Fix spacing in JSX

--- frontend-collab-todo-list/src/components/LoginForm.js
+++ frontend-collab-todo-list/src/components/LoginForm.js
@@ -2,29 +2,38 @@ import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 
-
 const LoginForm = ({ handleLogin, username, password, setPassword, setUsername, setCreateUser}) => (
   <>
     <Form>
       <Form.Group className="mx-auto w-50 mb-2">
         <Form.Label>Username</Form.Label>
-        <Form.Control type="text" placeholder="username" value={username} onChange={({target}) => setUsername(target.value)}/>
+        <Form.Control 
+          type="text" 
+          placeholder="username" 
+          value={username} 
+          onChange={({target}) => setUsername(target.value)}
+        />
       </Form.Group>
       <Form.Group className="mx-auto w-50 mb-2" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" value={password} onChange={({target}) => setPassword(target.value)}/>
+        <Form.Control 
+          type="password" 
+          placeholder="Password" 
+          value={password} 
+          onChange={({target}) => setPassword(target.value)}
+        />
       </Form.Group>
     </Form>
-  <Row className='w-25 mx-auto m-4'>
+    <Row className='w-25 mx-auto m-4'>
       <Button onClick={handleLogin} variant='primary'>
         Sign in
       </Button>
-  </Row>
-  <Row className='w-25 mx-auto m-4'>
+    </Row>
+    <Row className='w-25 mx-auto m-4'>
       <Button onClick={() => setCreateUser(true)} variant='outline-secondary'>
         Create Account
       </Button>
-  </Row>
+    </Row>
   </>
 )
 
