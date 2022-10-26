df2ff17b2c2cb1ffcc995fcd8bd028df59525d29
Michael Magen <michael.magen101@gmail.com>
2022-10-25 21:01:18 -0700

    Fix spacing and add commment in Header.js

--- frontend-collab-todo-list/src/components/Header.js
+++ frontend-collab-todo-list/src/components/Header.js
@@ -8,23 +8,24 @@ const Header = ({user, handleLogout}) => (
       <Navbar fixed='top' bg="dark" variant="dark" className='p-2' expand='lg'>
         <Container>
           <Navbar.Brand>Collaborative To-do List</Navbar.Brand>
-        {
-          user === null ?
-          <></> :
-          <>
-            <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
-            <Navbar.Collapse id='responsive-navbar-nav' className='justify-content-end'>
-              <Nav>
-                <Navbar.Text className='me-3'>
-                  Signed in as:  {user.username}
-                </Navbar.Text>
-                <Button variant='outline-danger' onClick={handleLogout}>
-                  Log Out
-                </Button>
-              </Nav>
-            </Navbar.Collapse>
-          </>
-        }
+          { 
+            // only show user info and logout if the user has loged in  
+            user === null ?
+            <></> :
+            <>
+              <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+              <Navbar.Collapse id='responsive-navbar-nav' className='justify-content-end'>
+                <Nav>
+                  <Navbar.Text className='me-3'>
+                    Signed in as:  {user.username}
+                  </Navbar.Text>
+                  <Button variant='outline-danger' onClick={handleLogout}>
+                    Log Out
+                  </Button>
+                </Nav>
+              </Navbar.Collapse>
+            </>
+          }
         </Container>
       </Navbar>
     </>
