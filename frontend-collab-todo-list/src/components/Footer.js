9d8593cc7f9d6f109dc9c5b45d926ba10fa20d3b
Michael Magen <michael.magen101@gmail.com>
2022-10-25 20:42:58 -0700

    Fix spacing in Footer.js

--- frontend-collab-todo-list/src/components/Footer.js
+++ frontend-collab-todo-list/src/components/Footer.js
@@ -7,15 +7,14 @@ const Footer = () => (
     <>
       <Navbar fixed='bottom' bg="dark" variant="dark" className='p-2'>
         <Stack gap={2} direction='horizontal' className="mx-auto">
-        <Navbar.Text>Created By Michael Magen</Navbar.Text>
-        <a href="https://github.com/michaelmagen/Collaborative-Todo-List">
-          <Button variant='light' className="rounded-pill">
-            <Github size={20} ></Github>
-          </Button>
-        </a>
+          <Navbar.Text>Created By Michael Magen</Navbar.Text>
+          <a href="https://github.com/michaelmagen/Collaborative-Todo-List">
+            <Button variant='light' className="rounded-pill">
+              <Github size={20} ></Github>
+            </Button>
+          </a>
         </Stack>
       </Navbar>
-      
     </>
 )
 
