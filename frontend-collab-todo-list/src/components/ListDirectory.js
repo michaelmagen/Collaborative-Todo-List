85c841c083a9d6a07e55ddec2aa2d5228ded6603
Michael Magen <michael.magen101@gmail.com>
2022-10-27 15:50:04 -0700

    Add style for form select

--- frontend-collab-todo-list/src/components/ListDirectory.js
+++ frontend-collab-todo-list/src/components/ListDirectory.js
@@ -1,15 +1,17 @@
+import Form from 'react-bootstrap/Form'
+
 const ListDirectory = ({lists, handleListChange, newList, setNewList, handleListAddition}) => {
   if (lists === null) {
     return (<></>)
   }
   return (
   <>
-    <h3>Select List</h3> 
-    <select onChange={handleListChange}>
+    <h4>Select List</h4> 
+    <Form.Select onChange={handleListChange} className='w-50'>
       <option value='none'>-- SELECT A LIST --</option>
       {lists.map(list => 
         <option value={list.id} key={list.id}> {list.title} </option>)}
-    </select>
+    </Form.Select>
     <br></br> 
   </>
   )
